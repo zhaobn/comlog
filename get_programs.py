2117e54521bcac240dd537a0555172b7a5702521
bonan <zbn.dale@gmail.com>
2021-02-28 17:30:30 +0000

    Refactor

--- get_programs.py
+++ get_programs.py
@@ -26,25 +26,11 @@ def add_to_program_lib(p_lib, entry_list):
       arg_types = et['arg_types']
       return_type = et['return_type']
       name = et['name'] if 'name' in et else ''
-    elif et.ctype in [ 'col', 'shp', 'pat', 'int', 'obj' ]:
-      # base types
-      terms = et.name
-      arg_types = ''
-      return_type = et.ctype
-      name = et.name
-    elif et.ctype == 'primitive':
-      terms = et.name
-      arg_types = args_to_string(et.arg_type)
-      return_type = et.return_type
-      name = et.name
-    elif et.ctype == 'program':
-      terms = names_to_string(secure_list(print_name(et.terms)))
-      arg_types = args_to_string(et.arg_type)
-      return_type = et.return_type
-      name = ''
     else:
-      print('Entry type not found!')
-      return(p_lib)
+      terms = et.name
+      arg_types = args_to_string(et.arg_type)
+      return_type = et.return_type
+      name = et.name
     # check existence
     program_found = p_lib.query(f'terms == "{terms}" & arg_types == "{arg_types}" & return_type == "{return_type}"')
     if program_found.empty:
@@ -65,7 +51,6 @@ def add_to_program_lib(p_lib, entry_list):
 
   return p_lib
 
-# %%
 df_pm = init_program_lib()
 df_pm = add_to_program_lib(df_pm, [
   getColor,
@@ -75,6 +60,7 @@ df_pm = add_to_program_lib(df_pm, [
   ifElse,
  ])
 
+ERROR_TERM = {'terms': 'ERROR', 'arg_types': '', 'return_type': '', 'name': 'ERROR'}
 # df_pm = add_to_program_lib(df_pm, {'terms':'[I]', 'arg_types': 'obj', 'return_type': 'obj', 'name': 'I'})
 # df_pm.to_csv('program_lib.csv')
 # df_pm = pd.read_csv('program_lib.csv')
@@ -85,10 +71,14 @@ def get_cached_program(type_signature, p_source = df_pm):
   matched_pms = p_source.query(f'arg_types == "{args_to_string(arg_t)}" & return_type == "{ret_t}"')
   return matched_pms if not matched_pms.empty else None
 
+def get_matched_program(return_type, p_source = df_pm):
+  matched_pms = p_source.query(f'return_type == "{return_type}"')
+  return matched_pms if not matched_pms.empty else None
+
 def sample_program(p_source):
   if p_source is None:
     print('No cache found!')
-    return {'terms': 'ERROR', 'arg_types': '', 'return_type': '', 'name': 'ERROR'}
+    return ERROR_TERM
   elif len(p_source.index) == 1:
     s_idx = 0
   else:
@@ -98,11 +88,23 @@ def sample_program(p_source):
   s_terms, s_arg_t, s_ret_t, = p_source.values[s_idx][:3]
   return {'terms': s_terms, 'arg_types': s_arg_t, 'return_type': s_ret_t}
 
-def get_matched_program(return_type, p_source = df_pm):
-  matched_pms = p_source.query(f'return_type == "{return_type}"')
-  return matched_pms if not matched_pms.empty else None
+# This is hacky. TODO: do it properly.
+def sample_primitive(type):
+  if type == 'col':
+    sampled_col = np.random.choice([Red, Blue, Yellow])
+    return {'terms': sampled_col.name, 'arg_types': '', 'return_type': 'col', 'name': sampled_col.name}
+  elif type == 'bool':
+    booleans = [
+      {'terms': 'True', 'arg_types': '', 'return_type': 'bool', 'name': 'True'},
+      {'terms': 'False', 'arg_types': '', 'return_type': 'bool', 'name': 'False'},
+    ]
+    return np.random.choice(booleans)
+  elif type == 'obj':
+    return {'terms': 'randomStone', 'arg_types': '', 'return_type': 'obj', 'name': 'randomStone'}
+  else:
+    print('Type not found!')
+    return ERROR_TERM
 
-# %%
 def sample_router(arg_list, free_index):
   assert len(arg_list) > 0, 'No arguments for router!'
   if free_index < 0:
@@ -110,11 +112,10 @@ def sample_router(arg_list, free_index):
   else:
     return ''.join([np.random.choice(['C', 'B', 'S']) for _ in arg_list])
 
-# %%
 def generate_program(type_signature, p_lib = df_pm, cur_step = 0, max_step = 5):
   if cur_step > max_step:
     print('Max step exceeded!')
-    return {'terms': 'EXCEED', 'arg_types': '', 'return_type': '', 'name': 'EXCEED'}
+    return ERROR_TERM
   elif type_signature == [['obj'], 'obj'] and np.random.random() < 0.5:
     return {'terms': 'I', 'arg_types': 'obj', 'return_type': 'obj'}
   else:
@@ -128,20 +129,7 @@ def generate_program(type_signature, p_lib = df_pm, cur_step = 0, max_step = 5):
       arg_t, ret_t = type_signature
       if len(arg_t) < 1:
         # return a primitive
-        if ret_t == 'col':
-          sampled_col = np.random.choice([Red, Blue, Yellow])
-          return {'terms': sampled_col.name, 'arg_types': '', 'return_type': 'col', 'name': sampled_col.name}
-        elif ret_t == 'bool':
-          booleans = [
-            {'terms': 'True', 'arg_types': '', 'return_type': 'bool', 'name': 'True'},
-            {'terms': 'False', 'arg_types': '', 'return_type': 'bool', 'name': 'False'},
-          ]
-          return np.random.choice(booleans)
-        elif ret_t == 'obj':
-          return {'terms': 'randomStone', 'arg_types': '', 'return_type': 'obj', 'name': 'randomStone'}
-        else:
-          print('Type not found!')
-          return {'terms': 'NOTYPE', 'arg_types': '', 'return_type': '', 'name': 'NOTYPE'}
+        return sample_primitive(ret_t)
       else:
         # generate new program
         left = sample_program(get_matched_program(ret_t, p_lib))
@@ -180,5 +168,4 @@ def expand_program(candidate, arg_list, free_index, p_lib, cur_step, max_step):
       'return_type': candidate['return_type']
     }
 
-# %%
 generate_program([['obj', 'obj'], 'obj'])
