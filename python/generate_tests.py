8586f422414069ae06a9ef35720d2ae510b45786
bonan <zbn.dale@gmail.com>
2021-05-18 10:43:02 +0100

    Set up new tests

--- python/generate_tests.py
+++ python/generate_tests.py
@@ -1,52 +1,19 @@
 # %%
 iter = 500
-# test_configs = {
-#   'moc': "task_data = task_row + task_col",
-#   'mor': "task_data = task_row + task_rdg",
-#   'mol': "task_data = task_row + task_ldg",
-#   'mco': "task_data = task_col + task_row",
-#   'mcr': "task_data = task_col + task_rdg",
-#   'mcl': "task_data = task_col + task_ldg",
-#   'mro': "task_data = task_rdg + task_row",
-#   'mrc': "task_data = task_rdg + task_col",
-#   'mrl': "task_data = task_rdg + task_ldg",
-#   'mlo': "task_data = task_ldg + task_row",
-#   'mlc': "task_data = task_ldg + task_col",
-#   'mlr': "task_data = task_ldg + task_rdg",
-#   'mcor': "task_data = task_col + task_row + task_rdg",
-# }
 test_configs = {
-  'ft_row': "task_data_df.at[i,'trial'] in [1,2,3]",
-  'ft_col': "task_data_df.at[i,'trial'] in [1,4,7]",
-  'ft_ldg': "task_data_df.at[i,'trial'] in [1,5,9]",
-  'ft_rdg': "task_data_df.at[i,'trial'] in [3,5,7]",
+  'nt_row': [0,1,2],
+  'nt_col': [0,3,6],
+  'nt_ldg': [0,4,8],
+  'nt_rdg': [2,4,6],
+  'nt_mrc': [0,1,2,3,6],
+  'nt_mcr': [0,3,6,1,2],
+  'nt_abr': [0,1,2,3,4,5,6,7,8],
+  'nt_abc': [0,3,6,1,4,7,2,5,8],
 }
 bash_scripts = ''
 
 # %%
 def generate_scripts(test_name, filter_condition, iteration=iter):
-#   defs = """
-# task_data_df = pd.read_csv('data/task_data.csv')
-
-# def get_task_task(tids, dsource=task_data_df):
-#   task_data = []
-#   for i in range(len(dsource)):
-#     if dsource.at[i,'phase']=='tab' and dsource.at[i,'trial'] in tids:
-#       tdata = dsource.iloc[i].to_dict()
-#       task = {
-#         'agent': eval(tdata['agent']),
-#         'recipient': eval(tdata['recipient']),
-#         'result': eval(tdata['result'])
-#       }
-#       task_data.append(task)
-#   return task_data
-
-# task_row = get_task_task([1,2,3])
-# task_col = get_task_task([1,4,7])
-# task_ldg = get_task_task([1,5,9])
-# task_rdg = get_task_task([3,5,7])
-
-#   """
   process_data = """
     tdata = task_data_df.iloc[i].to_dict()
     task = {
@@ -60,10 +27,12 @@ def generate_scripts(test_name, filter_condition, iteration=iter):
 from task import *
 
 task_data_df = pd.read_csv('data/task_data.csv')
+sorted_indexes = {filter_condition}
+
+task_data_df = task_data_df[task_data_df.index.isin(sorted_indexes)].reindex(sorted_indexes)
 task_data = []
 for i in range(len(task_data_df)):
-  if task_data_df.at[i,'phase']=='tab' and {filter_condition}:
-    {process_data}
+  {process_data}
 
 pm_init = pd.read_csv('data/task_pm.csv',index_col=0,na_filter=False)
 all_frames = pd.read_csv('data/task_frames.csv',index_col=0)
@@ -93,8 +62,8 @@ for item in test_configs.items():
 save_file('run.sh', bash_scripts)
 
 # %%
-for tn in ['bc', 'bh', 'bm', 'br']:
-  test_name = 'ts_'+tn
-  print(f'screen -dmS {test_name} python {test_name}.py')
+# for tn in ['bc', 'bh', 'bm', 'br']:
+#   test_name = 'ts_'+tn
+#   print(f'screen -dmS {test_name} python {test_name}.py')
 
 # %%
