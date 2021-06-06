207972777c1e999739de423051a8021ad7503121
sassy-2021 <zbn.dale@gmail.com>
2021-06-06 11:56:49 +0100

    Add new test

--- python/generate_tests.py
+++ python/generate_tests.py
@@ -1,16 +1,12 @@
 # %%
 iter = 500
 test_configs = {
-  # 'nt_row': [0,1,2],
-  # 'nt_col': [0,3,6],
-  # 'nt_ldg': [0,4,8],
-  # 'nt_rdg': [2,4,6],
-  # 'nt_mrc': [0,1,2,3,6],
-  # 'nt_mcr': [0,3,6,1,2],
-  # 'nt_abr': [0,1,2,3,4,5,6,7,8],
-  # 'nt_abc': [0,3,6,1,4,7,2,5,8],
-  'pt_r': [9,10,11,0,1,2],
-  'pt_c': [9,10,11,0,3,6],
+  'nd_r1': [0,1,2],
+  'nd_r2': [4,5,6],
+  'nd_c1': [1,4,7],
+  'nd_s4': [3,5,10],
+  'nd_d1': [0,5,9],
+  'nd_d2': [2,5,7],
 }
 bash_scripts = ''
 
@@ -28,7 +24,7 @@ def generate_scripts(test_name, filter_condition, iteration=iter):
   return f"""
 from task import *
 
-task_data_df = pd.read_csv('data/task_data.csv')
+task_data_df = pd.read_csv('data/task_data.csv', na_filter=False)
 sorted_indexes = {filter_condition}
 
 task_data_df = task_data_df[task_data_df.index.isin(sorted_indexes)].reindex(sorted_indexes)
@@ -39,10 +35,16 @@ for i in range(len(task_data_df)):
 pm_init = pd.read_csv('data/task_pm.csv',index_col=0,na_filter=False)
 all_frames = pd.read_csv('data/task_frames.csv',index_col=0)
 g = Task_gibbs(Task_lib(pm_init), task_data, iteration={iteration})
-g.fast_run(all_frames, save_prefix='{test_name}/{test_name}', sample=True, top_n=1)
+g.run(all_frames, save_prefix='{test_name}/{test_name}', sample=True, top_n=1)
 """
 
 def generate_cmd(test_name):
+  return f"""
+mkdir {test_name}
+screen -dmS {test_name} python {test_name}.py
+"""
+
+def generate_job(test_name):
   return f"""
 mkdir {test_name}
 screen -dmS {test_name} python {test_name}.py
@@ -54,18 +56,20 @@ def save_file(fname, fcontent, type='w'):
   f.close()
 
 
+# %%
+# for item in test_configs.items():
+#   (test_name, trial_cond) = item
+#   scripts = generate_scripts(test_name, trial_cond)
+#   # print(scripts)
+#   save_file(f'{test_name}.py', scripts)
+#   bash_scripts += generate_cmd(test_name)
+
+# save_file('run.sh', bash_scripts)
+
 for item in test_configs.items():
   (test_name, trial_cond) = item
   scripts = generate_scripts(test_name, trial_cond)
-  # print(scripts)
+  print(scripts)
   save_file(f'{test_name}.py', scripts)
-  bash_scripts += generate_cmd(test_name)
-
-save_file('run.sh', bash_scripts)
-
-# %%
-# for tn in ['bc', 'bh', 'bm', 'br']:
-#   test_name = 'ts_'+tn
-#   print(f'screen -dmS {test_name} python {test_name}.py')
 
 # %%
