c03f13b6daadb4f31c76f1860fce632afe79f323
sassy-2021 <zbn.dale@gmail.com>
2021-06-06 11:57:09 +0100

    Fix test run

--- python/test.py
+++ python/test.py
@@ -1,8 +1,8 @@
 
 from task import *
 
-task_data_df = pd.read_csv('data/task_data.csv')
-sorted_indexes = [0,1,2,5,8]
+task_data_df = pd.read_csv('data/task_data.csv', na_filter=False)
+sorted_indexes = [3,5,10]
 
 task_data_df = task_data_df[task_data_df.index.isin(sorted_indexes)].reindex(sorted_indexes)
 task_data = []
@@ -19,5 +19,5 @@ for i in range(len(task_data_df)):
 
 pm_init = pd.read_csv('data/task_pm.csv',index_col=0,na_filter=False)
 all_frames = pd.read_csv('data/task_frames.csv',index_col=0)
-g = Task_gibbs(Task_lib(pm_init), task_data, iteration=100)
-g.fast_run(all_frames, save_prefix='test/n', sample=True, top_n=1)
+g = Task_gibbs(Task_lib(pm_init), task_data, iteration=1)
+g.run(all_frames, save_prefix='test/n', sample=True, top_n=1)
