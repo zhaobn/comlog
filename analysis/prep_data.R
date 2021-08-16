703f6d1dff2f297ee1c9fee0fc9a493db8328553
sassy-2021 <zbn.dale@gmail.com>
2021-08-16 12:40:32 +0100

    [R] Analyze pilot 1 data

--- analysis/prep_data.R
+++ analysis/prep_data.R
@@ -2,7 +2,7 @@
 library(dplyr)
 rm(list=ls())
 
-load('data/pilot_1.rdata')
+load('data/pilot_1_raw.rdata')
 
 # Output to googlesheet to bonus participants
 to_sheet = df.sw %>%
@@ -10,3 +10,35 @@ to_sheet = df.sw %>%
 write.csv(to_sheet, file='data/pilot_1_responses.csv')
 
 # Clean up for analysis
+df.sw.raw = df.sw
+df.tw.raw = df.tw
+
+df.sw = df.sw.raw %>%
+  mutate(input_a = task.input.a_input, input_b = task.input.b_input,
+         certainty_a = task.input.a_certainty, certainty_b = task.input.b_certainty) %>%
+  select(ix, condition, date, time, instructions_duration, task_duration, 
+         age, sex, engagement, difficulty,
+         input_a, certainty_a, input_b, certainty_b, feedback,
+         correct) %>%
+  mutate(condition=as.character(condition),
+         age=as.numeric(as.character(age)),
+         engagement=as.numeric(as.character(engagement)),
+         difficulty=as.numeric(as.character(difficulty)),
+         certainty_a=as.numeric(as.character(certainty_a)),
+         certainty_b=as.numeric(as.character(certainty_b)))
+df.sw[3,'age'] = 42
+
+
+colnames(df.tw.raw)<-c('ix', 'id', 'batch', 'phase', 'trial', 'tid', 'agent', 'agent_color', 'recipient', 'result', 'selection', 'correct')
+df.tw = df.tw.raw %>%
+  mutate(batch=ifelse(batch=='alice', 'A', 'B')) %>%
+  filter(phase=='gen') %>%
+  select(ix, batch, trial, tid, agent, recipient, result, selection, correct)
+
+conditions = df.sw %>% select(ix, condition) 
+df.tw = df.tw %>%
+  left_join(conditions, by='ix')
+
+save(df.sw, df.tw, file='data/pilot_1_cleaned.rdata')
+
+
--- experiment/js/samples.js
+++ experiment/js/samples.js
@@ -1,8 +1,9 @@
 
 /** Prep data */
-const mode = 'dev'
-const cond = 'comp_mult'
+const mode = 'gen'
+const cond = 'comp_const'
 const taskIds = getConfigs(config, cond)
+
 
 let aliceLearn = fmtConfig(config.filter(c => taskIds['learnA'].indexOf(c.trial_id) > -1), 'alice', 'learn')
 let aliceGen = fmtConfig(shuffleArray(config.filter(c => taskIds['genA'].indexOf(c.trial_id) > -1)), 'alice', 'gen')
@@ -13,17 +14,34 @@ let bobGen = fmtConfig(shuffleArray(config.filter(c => taskIds['genB'].indexOf(c
 bobGen.map(bg => bg.trial = bg.trial + aliceGen.length)
 
 let sampleBase = document.getElementById('samples')
-
 let aliceSummaryDiv = createCustomElement('div', 'summary-div', 'alice-summary-div');
 let bobSummaryDiv = createCustomElement('div', 'summary-div', 'bob-summary-div');
 
-for(let i = 0; i < aliceLearn.length; i++ ) {
-  aliceSummaryDiv.append(createSum(aliceLearn[i], 'alice-sum'))
+
+if (mode==='gen') {
+  let genConfigs = aliceGen
+  genConfigs = genConfigs.sort((a, b) => parseInt(a.id.slice(1,)) - parseInt(b.id.slice(1,)))
+  genConfigs.map(gc => gc.result = gc.recipient)
+
+  console.log(genConfigs)
+  let midPoint = Math.ceil(aliceGen.length/2)
+  for(let i = 0; i < midPoint; i++ ) {
+    aliceSummaryDiv.append(createSum(aliceGen[i], 'alice-sum'))
+  }
+  for(let i = midPoint; i < aliceGen.length; i++ ) {
+    bobSummaryDiv.append(createSum(aliceGen[i], 'bob-sum'))
+  }
+
+} else {
+  for(let i = 0; i < aliceLearn.length; i++ ) {
+    aliceSummaryDiv.append(createSum(aliceLearn[i], 'alice-sum'))
+  }
+  for(let i = 0; i < bobLearn.length; i++ ) {
+    bobSummaryDiv.append(createSum(bobLearn[i], 'bob-sum'))
+  }
 }
 
-for(let i = 0; i < bobLearn.length; i++ ) {
-  bobSummaryDiv.append(createSum(bobLearn[i], 'bob-sum'))
-}
+
 
 sampleBase.append(aliceSummaryDiv);
 sampleBase.append(bobSummaryDiv);
