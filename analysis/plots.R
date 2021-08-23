2af56fb1f0e45ffdee1da10df36c0f0122bdb19a
sassy-2021 <zbn.dale@gmail.com>
2021-08-23 12:00:04 +0100

    Plot model preds

--- analysis/plots.R
+++ analysis/plots.R
@@ -38,7 +38,7 @@ df.tw %>%
 df.tw %>%
   mutate(trial=as.factor(as.character(trial))) %>%
   ggplot( aes(y=trial, x=prediction, fill=trial)) +
-  geom_density_ridges(alpha=0.6, stat="binline", bins=20) +
+  geom_density_ridges(alpha=0.6, stat="binline", bins=20, scale=0.95) +
   geom_point(data=answers) +
   scale_x_discrete(limits=c(0,seq(max(df.tw$prediction)))) +
   scale_y_discrete(limits=rev) +
@@ -48,23 +48,50 @@ df.tw %>%
 
 
 # Plot model preds
-preds.combine = read.csv('preds/combine_preds_a.csv')
-preds.combine = preds.combine %>%
-  filter(substr(terms, 1, 11)=='Stone(S0,O0') %>%
+preds.a = read.csv('preds/combine_preds_a.csv')
+preds.a = preds.a %>%
   select(terms, starts_with('prob')) %>%
   gather(trial, value, starts_with('prob')) %>%
-  mutate(terms=as.character(terms),
-         trial=as.character(trial)) %>%
-  mutate(blocks=as.numeric(substr(terms, 14, nchar(terms)-1)),
-         trial=as.numeric(substr(trial, 6, nchar(trial)))) %>%
-  select(trial, prediction=blocks, prob=value) %>%
+  mutate(terms=terms+1,
+         trial=as.numeric(substr(trial, 6, nchar(trial))),
+         phase='A') %>%
+  select(phase, trial, prediction=terms, value) %>%
   arrange(trial, prediction)
 
+preds.b = read.csv('preds/combine_preds_b.csv')
+preds.b = preds.b %>%
+  select(terms, starts_with('prob')) %>%
+  gather(trial, value, starts_with('prob')) %>%
+  mutate(terms=terms+1,
+         trial=as.numeric(substr(trial, 6, nchar(trial))),
+         phase='B') %>%
+  select(phase, trial, prediction=terms, value) %>%
+  arrange(trial, prediction)
+
+preds.combine = rbind(preds.a, preds.b) %>%
+  mutate(trial=as.factor(as.character(trial)))
+
+
 preds.combine %>%
-  filter(prob>0) %>%
-  mutate(trial=as.factor(as.character(trial))) %>%
-  ggplot( aes(y=trial, x=prediction, fill=trial)) +
-  geom_density_ridges(alpha=0.6, stat="binline", bins=20) +
+  ggplot( aes(y=trial, x=prediction, height=value, fill=trial)) +
+  geom_density_ridges(stat="identity", alpha=0.6) +
+  scale_x_discrete(limits=factor(c(0,seq(9)))) +
+  scale_y_discrete(limits=rev) +
+  theme_bw() +
+  theme(legend.position = 'none') +
+  facet_wrap(~phase)
+
+
+
+
+
+
+
+# Not working for now
+preds.combine %>%
+  mutate(trial=as.factor(as.character(trial)), prob=value) %>%
+  ggplot(aes(y=trial, x=prediction, height=stat(prob))) +
+  geom_density_ridges(stat = "binline", bins = 20, scale = 0.95, draw_baseline = FALSE) +
   scale_x_discrete(limits=factor(c(0,seq(9)))) +
   scale_y_discrete(limits=rev) +
   theme_bw()
@@ -72,8 +99,23 @@ preds.combine %>%
 
 
 
+d <- data.frame(
+  x = rep(1:5, 3),
+  y = c(rep(0, 5), rep(1, 5), rep(2, 5)),
+  height = c(0, 1, 3, 4, 0, 1, 2, 3, 5, 4, 0, 5, 4, 4, 1)
+)
+ggplot(d, aes(x, y, height = height, group = y)) + 
+  geom_ridgeline(fill = "lightblue")
 
+preds.combine %>%
+  mutate(trial=as.factor(as.character(trial)), height=value) %>%
+  ggplot( aes(x=prediction,y=trial, height=height, group=trial)) +
+  geom_density_ridges(stat = "identity")
 
+preds.combine = preds.combine %>%
+  mutate(trial=as.factor(as.character(trial)), height=value)
+ggplot(preds.combine, aes(x=prediction, y=trial, height=stat(value))) +
+  geom_density_ridges(stat = "binline", bins = 20, scale = 0.95, draw_baseline = FALSE)
 
 
 
