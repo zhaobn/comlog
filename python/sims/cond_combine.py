2facca9c7f7578ed56748f9b8939e4016b95b069
sassy-2021 <zbn.dale@gmail.com>
2021-08-26 20:57:12 +0100

    Update tests

--- python/sims/cond_combine.py
+++ python/sims/cond_combine.py
@@ -42,21 +42,21 @@ data_len_b = len(task_data['learn_b'])
 # Learning phase A
 pl = Task_lib(pd.read_csv('../data/task_pm.csv', index_col=0, na_filter=False))
 g1 = Task_gibbs(pl, task_data['learn_a'], iteration=LEARN_ITER)
-g1.run(all_frames, top_n=TOP_N, save_prefix=f'{SAVE_DIR}_a')
+g1.run(all_frames, top_n=TOP_N, exceptions_allowed=EXCEPTS, save_prefix=f'{SAVE_DIR}_a')
 
 
 # Gen predictions A
-a_learned = pd.read_csv(f'{SAVE_DIR}_a_lib_{str(LEARN_ITER+1).zfill(padding)}_{str(data_len_a+1).zfill(padding)}.csv', index_col=0, na_filter=False)
+a_learned = pd.read_csv(f'{SAVE_DIR}_a_lib_{str(LEARN_ITER).zfill(padding)}_{str(data_len_a).zfill(padding)}.csv', index_col=0, na_filter=False)
 a_gen = sim_for_all(task_data['gen'],  Task_lib(a_learned), 1000)
 a_gen.to_csv('construct_preds_a.csv')
 
 
 # Learning phase B
 g2 = Task_gibbs(Task_lib(a_learned), task_data['learn_b'], iteration=LEARN_ITER)
-g2.run(all_frames, top_n=TOP_N, exceptions_allowed=1, save_prefix=f'{SAVE_DIR}_b')
+g2.run(all_frames, top_n=TOP_N, exceptions_allowed=EXCEPTS, save_prefix=f'{SAVE_DIR}_b')
 
 
 # Gen predictions B
-b_learned = pd.read_csv(f'{SAVE_DIR}_b_lib_{str(LEARN_ITER+1).zfill(padding)}_{str(data_len_b+1).zfill(padding)}.csv', index_col=0, na_filter=False)
+b_learned = pd.read_csv(f'{SAVE_DIR}_b_lib_{str(LEARN_ITER).zfill(padding)}_{str(data_len_b).zfill(padding)}.csv', index_col=0, na_filter=False)
 b_gen = sim_for_all(task_data['gen'], Task_lib(b_learned), 1000)
 b_gen.to_csv('construct_preds_b.csv')
