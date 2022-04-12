b8925995143ff0c174d2b09dc27436ad800a286c
bonan <zbn.dale@gmail.com>
2022-04-12 21:09:13 +0100

    [models] WIP: similarity model

--- analysis/alt_models/sim_model.R
+++ analysis/alt_models/sim_model.R
@@ -1,12 +1,11 @@
 
 library(dplyr)
 
-normalize<-function(vec) {
-  return (vec/sum(vec))
-}
+normalize<-function(vec) return (vec/sum(vec))
 
 #### Experiment 1 ####
 task = read.csv('../../data/tasks/exp_1.csv') %>% select(-X)
+load('../../data/exp_1_cleaned.rdata')
 
 # Measure similarity
 calc_sim <- function(dx, dy, ws=1, wd=1, wb=1) {
@@ -15,7 +14,7 @@ calc_sim <- function(dx, dy, ws=1, wd=1, wb=1) {
 }
 
 # Generalization preds
-get_trial_pred <- function(cond, tid, batch) {
+get_trial_pred <- function(cond, tid, batch, pars=list(ws=1, wd=1, wb=1)) {
   all_terms = data.frame(terms=c(0, seq(16)))
   rt_colname = paste0('prob_', tid)
   
@@ -31,7 +30,7 @@ get_trial_pred <- function(cond, tid, batch) {
   sim_df = data.frame(lid=seq(nrow(trainings)), terms=0, sim_score=0)
   for (row in 1:nrow(sim_df)) {
     sim_df[row, 'terms'] = trainings[row,'result_block']
-    sim_df[row, 'sim_score'] = calc_sim(as.list(trainings[row,]), task_data)
+    sim_df[row, 'sim_score'] = calc_sim(as.list(trainings[row,]), task_data, pars[['ws']], pars[['wd']], pars[['wb']])
     
   }
   sim_df = sim_df %>% group_by(terms) %>% summarise(weight=sum(sim_score))
@@ -50,21 +49,36 @@ get_trial_pred <- function(cond, tid, batch) {
 # Get results
 condition='construct'
 batch='B'
-filename=paste0('sim_model_preds/exp_1/', condition, '_preds_', tolower(batch), '.csv')
+model='sim'
+experi='exp_1'
 
-preds = get_trial_pred(condition, 1, batch)
-for (tid in seq(8)) {
-  if (tid>1) {
-    preds = preds %>% left_join(get_trial_pred(condition, tid, batch), by='terms')
-  }
-}
-
-write.csv(preds, filename)
+preds = get_trial_pred(condition, 1, batch, list(ws=0, wd=1, wb=0))
+# for (tid in seq(8)) {
+#   if (tid>1) {
+#     preds = preds %>% left_join(get_trial_pred(condition, tid, batch), by='terms')
+#   }
+# }
+# filename=paste0('sim_model_preds/', experi, '_' condition, '_preds_', tolower(batch), '.csv') 
+# write.csv(preds, filename)
 
 # Fit parameters
+ppt_list = expand.grid(condition=cond, phase=toupper(phase), trial=seq(8), term=seq(0,16))
+ppt_data = df.tw %>% 
+  filter(condition==cond, batch==toupper(phase)) %>%
+  mutate(phase=batch, term=prediction) %>%
+  count(condition, phase, trial, term) %>%
+  right_join(ppt_list, by=c('condition', 'phase', 'trial', 'term')) %>%
+  mutate(n=ifelse(is.na(n), 0, n)) %>%
+  arrange(condition, phase, trial, term)
+
+data = ppt_data%>%filter(condition=='construct', phase=='A', trial!=3)
+wg_sims<-function(data, par) {
+  
+  
+}
 
 # Get fitted predictions
 
-# Plots
+
 
 #### End of Experiment 1
