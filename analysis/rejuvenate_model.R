82f452ee52cfe4f568246006320c20023c6baf1e
bonan <zbn.dale@gmail.com>
2022-06-07 11:26:27 +0100

    More data

--- analysis/rejuvenate_model.R
+++ analysis/rejuvenate_model.R
@@ -2,12 +2,69 @@
 library(dplyr)
 library(tidyr)
 
-model='pcfg'
-eid=1
-cond = 'construct'
-rj_eid = 1
-rj_cond = 'decon'
+models = c('ag', 'pcfg')
+iters = c(10,50,100, seq(200,1000,200), seq(2000,10000,2000), seq(20000,100000,20000))
+options(scipen=999)
 
+setups = list(
+  list(cond = 'construct', rj_cond = 'decon'),
+  list(cond = 'decon', rj_cond = 'construct'),
+  list(cond = 'combine', rj_cond = 'flip'),
+  list(cond = 'flip', rj_cond = 'combine')
+) 
+
+get_reju_mix = function(model, iter, weight=0.5) {
+  
+  for (pair in setups) {
+    
+    forward_preds = read.csv(file = paste0(
+      '../model_data/', tolower(model), '/process_', iter, '/', pair[['cond']], '_preds_b.csv' 
+    )) %>% 
+      select(terms, starts_with('prob_')) %>%
+      gather(trial, prob, -terms) %>%
+      mutate(trial=as.numeric(substr(trial, 6, nchar(trial))))
+    
+    backward_preds = read.csv(file = paste0(
+      '../model_data/', tolower(model), '/process_', iter, '/', pair[['rj_cond']], '_preds_b.csv' 
+    )) %>% 
+      select(terms, starts_with('prob_')) %>%
+      gather(trial, bprob, -terms) %>%
+      mutate(trial=as.numeric(substr(trial, 6, nchar(trial))))
+    
+    backward_weight = weight/(1+weight)
+    forward_weight = 1-backward_weight
+    
+    rj_preds = forward_preds %>%
+      left_join(backward_preds, by=c('terms', 'trial')) %>%
+      mutate(rprob=forward_weight*prob+backward_weight*bprob) 
+    rj_preds_fmt = rj_preds %>%
+      select(terms, trial, rprob) %>%
+      spread(trial, rprob)
+    colnames(rj_preds_fmt) = c('terms', paste0('prob_', seq(8)))
+    
+    write.csv(rj_preds_fmt, file = paste0(
+      '../model_data/', tolower(model), '_rj/process_', iter, '/', pair[['rj_cond']], '_preds_b.csv' 
+    )) 
+  }
+}
+
+for (m in models) {
+  for (i in iters) {
+    get_reju_mix(m, i)
+  }
+}
+
+
+
+
+
+
+#### ARCHIVE: with exp prefixes ####
+
+eid=1
+cond='construct'
+rj_eid=1
+rj_cond='decon'
 
 forward_preds = read.csv(file = paste0(
   '../model_data/', tolower(model), '/exp_', eid, '/', cond, '_preds_b.csv' 
---
+++ job.py
@@ -0,0 +1,22 @@
+
+# %%
+LEARN_ITERS = [10,50,100] + list(range(200, 1001, 200)) + list(range(2000, 10001, 2000)) + list(range(20000, 100001, 20000))
+
+
+# %%
+for i in LEARN_ITERS:
+  print(f'mkdir process_{str(i)}')
+  # print(f'cp ag/decon_preds_a.csv ag/process_{str(i)}/decon_preds_a.csv')
+  # print(f'cp ag/decon_preds_b.csv ag/process_{str(i)}/decon_preds_b.csv')
+
+# %%
+TO_CP_ITERS = list(range(4000, 10001, 2000)) + list(range(20000, 100001, 20000))
+for i in TO_CP_ITERS:
+  print(f'cp -r process_4000/ process_{str(i)}/')
+
+# %%
+for i in LEARN_ITERS:
+  for c in ['construct', 'combine', 'decon', 'flip']:
+    print(f'cp pcfg/process_{str(i)}/{c}_preds_a.csv pcfg_rj/process_{str(i)}/{c}_preds_a.csv')
+
+# %%
