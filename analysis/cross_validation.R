f51501bde227924c7ef71ee14f974ca0b07dea5b
bonan <zbn.dale@gmail.com>
2022-06-09 18:51:45 +0100

    Run sims

--- analysis/cross_validation.R
+++ analysis/cross_validation.R
@@ -5,6 +5,159 @@ library(tidyr)
 library(ggplot2)
 
 
+#### Play with ag data ####
+load('../data/all_cleaned.Rdata')
+ppt_data = df.tw %>% count(exp_id, condition, batch, trial, prediction)
+ppt_data = ppt_data %>%
+  group_by(condition, batch, trial, prediction) %>%
+  summarise(n=sum(n))
+
+## Helper functions
+sigmoid = function(x) return(1/(1+exp(-x)))
+NCHUNK=17
+
+hazdata = function(data, par) {
+  data$transformed = sigmoid(par)/NCHUNK+(1-sigmoid(par))*data$prob
+  data$ll = log(data$transformed)*data$n
+  return(data)
+}
+hazfit = function(data, par) {
+  data$transformed = sigmoid(par)/NCHUNK+(1-sigmoid(par))*data$prob
+  data$ll = log(data$transformed)*data$n
+  return(-sum(data$ll))
+}
+
+
+## Setup
+iters = seq(100,1500,100)
+
+## Fit
+conditions = c('construct','combine','decon','flip')
+batches = c('a','b')
+fit_pred = function(model, iter) {
+  # Get model data
+  model_data = read.csv(text='condition,batch,trial,prediction,prob')
+  for (cond in conditions) {
+    for (batch in batches) {
+      model_raw=read.csv(paste0('../model_data/',tolower(model),'/process_',iter,'/',cond,'_preds_',tolower(batch),'.csv'))
+      model_raw = model_raw %>%
+        select(term=terms, starts_with('prob')) %>%
+        gather('trial', 'prob', -term) %>%
+        mutate(trial=as.numeric(substr(trial, 6, nchar(trial))), 
+               prediction=term, condition=cond, batch=toupper(batch)) %>%
+        select(condition, batch, trial, prediction, prob)
+      
+      model_data = rbind(model_data, model_raw)
+    }
+  }
+  
+  # Add ppt data
+  model_ppt=model_data %>%
+    left_join(ppt_data, by=c('condition', 'batch', 'trial', 'prediction')) %>%
+    mutate(n=ifelse(is.na(n),0, n)) %>%
+    arrange(condition, batch, trial, prediction)
+  
+  # Fit
+  out = optim(par=0, hazfit, method="L-BFGS-B", data=model_ppt)
+  
+  # Return
+  return (list(model=toupper(model), iter=iter, fit=out$value, param=out$par))
+}
+
+
+## Fit all
+fit_results = read.csv(file='cross_valids/processes.csv') %>% select(-X)
+for (i in iters) {
+  fit_results = rbind(fit_results,data.frame(fit_pred('ag',i)))
+  fit_results = rbind(fit_results,data.frame(fit_pred('agr',i)))
+}
+#write.csv(fit_results, file='cross_valids/processes.csv')
+
+
+## Quick plot
+fit_results %>%
+  #filter(iter>101, iter<80001) %>%
+  ggplot(aes(x=iter, y=-fit, color=model)) +
+  geom_line()
+
+
+
+#### Play with pcfg data ####
+load('../data/all_cleaned.Rdata')
+ppt_data = df.tw %>% count(exp_id, condition, batch, trial, prediction)
+ppt_data = ppt_data %>%
+  group_by(condition, batch, trial, prediction) %>%
+  summarise(n=sum(n))
+
+## Helper functions
+sigmoid = function(x) return(1/(1+exp(-x)))
+NCHUNK=17
+
+hazdata = function(data, par) {
+  data$transformed = sigmoid(par)/NCHUNK+(1-sigmoid(par))*data$prob
+  data$ll = log(data$transformed)*data$n
+  return(data)
+}
+hazfit = function(data, par) {
+  data$transformed = sigmoid(par)/NCHUNK+(1-sigmoid(par))*data$prob
+  data$ll = log(data$transformed)*data$n
+  return(-sum(data$ll))
+}
+
+
+## Setup
+iters = c(seq(100,1000,100), seq(2000,10000,1000))
+
+## Fit
+conditions = c('construct','combine','decon','flip')
+batches = c('a','b')
+fit_pred = function(model, iter) {
+  # Get model data
+  model_data = read.csv(text='condition,batch,trial,prediction,prob')
+  for (cond in conditions) {
+    for (batch in batches) {
+      model_raw=read.csv(paste0('../model_data/',tolower(model),'/process_',iter,'/',cond,'_preds_',tolower(batch),'.csv'))
+      model_raw = model_raw %>%
+        select(term=terms, starts_with('prob')) %>%
+        gather('trial', 'prob', -term) %>%
+        mutate(trial=as.numeric(substr(trial, 6, nchar(trial))), 
+               prediction=term, condition=cond, batch=toupper(batch)) %>%
+        select(condition, batch, trial, prediction, prob)
+      
+      model_data = rbind(model_data, model_raw)
+    }
+  }
+  
+  # Add ppt data
+  model_ppt=model_data %>%
+    left_join(ppt_data, by=c('condition', 'batch', 'trial', 'prediction')) %>%
+    mutate(n=ifelse(is.na(n),0, n)) %>%
+    arrange(condition, batch, trial, prediction)
+  
+  # Fit
+  out = optim(par=0, hazfit, method="L-BFGS-B", data=model_ppt)
+  
+  # Return
+  return (list(model=toupper(model), iter=iter, fit=out$value, param=out$par))
+}
+
+
+## Fit all
+fit_results = read.csv(text='model,iter,fit,param')
+for (i in iters) {
+  fit_results = rbind(fit_results,data.frame(fit_pred('pcfg',i)))
+}
+write.csv(fit_results, file='cross_valids/processes.csv')
+
+
+## Quick plot
+fit_results %>%
+  #filter(iter>101, iter<80001) %>%
+  ggplot(aes(x=iter, y=-fit, color=model)) +
+  geom_line()
+
+
+
 #### Overall fits per iteration ####
 ## Load mturk data and aggregate per condition
 load('../data/all_cleaned.Rdata')
--- analysis/rejuvenate_model.R
+++ analysis/rejuvenate_model.R
@@ -2,8 +2,7 @@
 library(dplyr)
 library(tidyr)
 
-models = c('ag', 'pcfg')
-iters = c(10,50,100, seq(200,1000,200), seq(2000,10000,2000), seq(20000,100000,20000))
+iters = seq(100,1500,100)
 options(scipen=999)
 
 setups = list(
@@ -43,15 +42,14 @@ get_reju_mix = function(model, iter, weight=0.5) {
     colnames(rj_preds_fmt) = c('terms', paste0('prob_', seq(8)))
     
     write.csv(rj_preds_fmt, file = paste0(
-      '../model_data/', tolower(model), '_rj/process_', iter, '/', pair[['rj_cond']], '_preds_b.csv' 
+      '../model_data/', tolower(model), 'r/process_', iter, '/', pair[['rj_cond']], '_preds_b.csv' 
     )) 
   }
 }
 
-for (m in models) {
-  for (i in iters) {
-    get_reju_mix(m, i)
-  }
+
+for (i in iters) {
+  get_reju_mix('ag', i)
 }
 
 
@@ -59,40 +57,40 @@ for (m in models) {
 
 
 
-#### ARCHIVE: with exp prefixes ####
-
-eid=1
-cond='construct'
-rj_eid=1
-rj_cond='decon'
-
-forward_preds = read.csv(file = paste0(
-  '../model_data/', tolower(model), '/exp_', eid, '/', cond, '_preds_b.csv' 
-)) %>% 
-  select(terms, starts_with('prob_')) %>%
-  gather(trial, prob, -terms) %>%
-  mutate(trial=as.numeric(substr(trial, 6, nchar(trial))))
-
-backward_preds = read.csv(file = paste0(
-  '../model_data/', tolower(model), '/exp_', rj_eid, '/', rj_cond, '_preds_b.csv' 
-)) %>% 
-  select(terms, starts_with('prob_')) %>%
-  gather(trial, bprob, -terms) %>%
-  mutate(trial=as.numeric(substr(trial, 6, nchar(trial))))
-
-rj_preds = forward_preds %>%
-  left_join(backward_preds, by=c('terms', 'trial')) %>%
-  mutate(rprob=0.714*prob+0.286*bprob) 
-rj_preds_fmt = rj_preds %>%
-  select(terms, trial, rprob) %>%
-  spread(trial, rprob)
-colnames(rj_preds_fmt) = c('terms', paste0('prob_', seq(8)))
-
-write.csv(rj_preds_fmt, file = paste0(
-  '../model_data/', tolower(model), '_rj/exp_', eid, '/', cond, '_preds_b.csv' 
-))
-
-
+# #### ARCHIVE: with exp prefixes ####
+# 
+# eid=1
+# cond='construct'
+# rj_eid=1
+# rj_cond='decon'
+# 
+# forward_preds = read.csv(file = paste0(
+#   '../model_data/', tolower(model), '/exp_', eid, '/', cond, '_preds_b.csv' 
+# )) %>% 
+#   select(terms, starts_with('prob_')) %>%
+#   gather(trial, prob, -terms) %>%
+#   mutate(trial=as.numeric(substr(trial, 6, nchar(trial))))
+# 
+# backward_preds = read.csv(file = paste0(
+#   '../model_data/', tolower(model), '/exp_', rj_eid, '/', rj_cond, '_preds_b.csv' 
+# )) %>% 
+#   select(terms, starts_with('prob_')) %>%
+#   gather(trial, bprob, -terms) %>%
+#   mutate(trial=as.numeric(substr(trial, 6, nchar(trial))))
+# 
+# rj_preds = forward_preds %>%
+#   left_join(backward_preds, by=c('terms', 'trial')) %>%
+#   mutate(rprob=0.714*prob+0.286*bprob) 
+# rj_preds_fmt = rj_preds %>%
+#   select(terms, trial, rprob) %>%
+#   spread(trial, rprob)
+# colnames(rj_preds_fmt) = c('terms', paste0('prob_', seq(8)))
+# 
+# write.csv(rj_preds_fmt, file = paste0(
+#   '../model_data/', tolower(model), '_rj/exp_', eid, '/', cond, '_preds_b.csv' 
+# ))
+# 
+# 
 
 
 
